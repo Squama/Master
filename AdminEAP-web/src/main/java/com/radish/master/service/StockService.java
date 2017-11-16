c0b709ac88dc0cad295469cedd9bd842676f7b66
28445 <284454372@qq.com>
2017-11-16 13:06:48 +0800

    输入校验新增，调度完善

--- AdminEAP-web/src/main/java/com/radish/master/service/StockService.java
+++ AdminEAP-web/src/main/java/com/radish/master/service/StockService.java
@@ -3,11 +3,12 @@ package com.radish.master.service;
 
 import com.cnpc.framework.base.service.BaseService;
 import com.radish.master.entity.Project;
+import com.radish.master.entity.Stock;
 
 
 public interface StockService extends BaseService {
 
     Boolean saveHistory(String project_ID,String mat_ID,int stockChangeNum,String useTpye,String stockSource,String remark);
-    Boolean stockChange(String budget_ID,String mat_ID,int stockChangeNum,int changeType);
+    Boolean stockChange(String project_ID,String mat_ID,int stockChangeNum,int changeType);
     Project getProjectByBudget(String budget_ID);
 }
