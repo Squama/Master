c0a034c90f58836507a618ef094dd757d6f53ce5
Squama <tonydong20@gmail.com>
2018-04-08 18:07:14 +0800

    简单提交

---
+++ AdminEAP-web/src/main/java/com/radish/master/entity/mech/MechBudgetDetail.java
@@ -0,0 +1,20 @@
+/**
+ * 版权所有 (c) 2018，中金支付有限公司  
+ */
+package com.radish.master.entity.mech;
+
+/**
+* 类说明
+* 
+* <pre>
+* Modify Information:
+* Author        Date          Description
+* ============ =========== ============================
+* dongyan      2018年4月8日    Create this file
+* </pre>
+* 
+*/
+
+public class MechBudgetDetail {
+
+}
