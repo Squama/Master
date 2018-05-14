e1b43e5c6794ef9532fb9defd165f60a4991cc8d
wzh0209 <32840049+wzh0209@users.noreply.github.com>
2018-05-14 22:21:15 +0800

    清单支付改后版本1

--- AdminEAP-web/src/main/java/com/radish/master/controller/volumePay/VolumePayController.java
+++ AdminEAP-web/src/main/java/com/radish/master/controller/volumePay/VolumePayController.java
@@ -1,7 +1,9 @@
 package com.radish.master.controller.volumePay;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +12,13 @@ import org.hibernate.type.StringType;
 import org.hibernate.type.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONArray;
+import com.cnpc.framework.activiti.pojo.Constants;
+import com.cnpc.framework.activiti.service.RuntimePageService;
 import com.cnpc.framework.base.entity.User;
 import com.cnpc.framework.base.pojo.Result;
 import com.cnpc.framework.base.service.BaseService;
@@ -22,6 +27,7 @@ import com.radish.master.entity.Labor;
 import com.radish.master.entity.ProjectVolume;
 import com.radish.master.entity.project.Salary;
 import com.radish.master.entity.project.SalaryDetail;
+import com.radish.master.entity.project.SalaryVolume;
 import com.radish.master.entity.volumePay.VolumePay;
 import com.radish.master.service.BudgetService;
 import com.radish.master.system.Arith;
@@ -32,6 +38,9 @@ public class VolumePayController {
 	private String prefix="/VolumePay/";
 	@Resource
     private BudgetService budgetService;
+	 @Resource
+	 private RuntimePageService runtimePageService;
+	
 	@Autowired
 	private BaseService baseService;
 	
@@ -40,6 +49,173 @@ public class VolumePayController {
 		request.setAttribute("projectOptions", JSONArray.toJSONString(budgetService.getProjectCombobox()));
 		return prefix+"list";
 	}
+	@RequestMapping("/payList")
+	public String payList(HttpServletRequest request){
+		Arith air = new Arith();
+		String gclid = request.getParameter("gclid");
+		String type = request.getParameter("type");
+		request.setAttribute("gclid", gclid);
+		request.setAttribute("type", type);
+		request.setAttribute("zdr", SecurityUtil.getUserId());
+		if("10".equals(type)){
+			List<SalaryVolume> gxs = baseService.findBySql("select * from tbl_salary_volume where volume_id='"+gclid+"'", SalaryVolume.class);
+			if(gxs.size()>0){
+				Salary gz = baseService.get(Salary.class, gxs.get(0).getSalaryID());
+				if("60".equals(gz.getStatus())){
+					request.setAttribute("gz", JSONArray.toJSONString(gz));
+					List<SalaryDetail> gzmxs = baseService.findBySql(" select * from tbl_salary_detail where salary_id ='"+gz.getId()+"'", SalaryDetail.class);
+					double zje = 0.0;
+					for(SalaryDetail gzmx:gzmxs){
+						zje =air.add(zje, Double.valueOf(gzmx.getActual()));
+					}
+					String sql = " select * from tbl_volumePay where volumeId='"+gz.getId()+"' and payType='10' and status<>'40'";
+					List<VolumePay> zfs = baseService.findBySql(sql, VolumePay.class);
+					double kzf = zje;
+					for(VolumePay zf:zfs){
+						kzf = air.sub(kzf, Double.valueOf(zf.getPayMoney()));
+					}
+					request.setAttribute("zje", zje);
+					request.setAttribute("kzf", kzf);
+				}
+				
+			}
+			return prefix+"payListRg";
+		}
+		return prefix+"payList";
+	}
+	@RequestMapping("/addPayInfo")//非人工支付
+	public String addPayInfo(HttpServletRequest request){
+		Arith air = new Arith();
+		List<User> ul1 = baseService.findMapBySql("select u.name name ,u.id id from tbl_user u ", new Object[]{}, new Type[]{StringType.INSTANCE}, User.class);
+		request.setAttribute("use", JSONArray.toJSONString(ul1));
+		String gclid = request.getParameter("gclid");
+		String type = request.getParameter("type");
+		request.setAttribute("gclid", gclid);
+		request.setAttribute("type", type);
+		
+		String zfid = request.getParameter("zfid");
+		String lx = request.getParameter("lx");
+		request.setAttribute("zfid", zfid);
+		request.setAttribute("lx", lx);
+		
+		//算出总金额和可支付金额
+		ProjectVolume pv = baseService.get(ProjectVolume.class, gclid);
+		Labor ht = baseService.get(Labor.class, pv.getLaborID());
+		request.setAttribute("bm", pv.getProjectName()+"--"+ht.getConstructionTeam());
+		String sql = " select * from tbl_volumePay where volumeId='"+gclid+"' and payType='"+type+"' and status<>'40'";
+		List<VolumePay> zfs = baseService.findBySql(sql, VolumePay.class);
+		double zje = 0.0;
+		if("20".equals(type)){
+			zje = Double.valueOf(pv.getFinalMech());
+		}else if("30".equals(type)){
+			zje = Double.valueOf(pv.getFinalMat());
+		}
+		double kzf = zje;
+		for(VolumePay zf:zfs){
+			kzf = air.sub(kzf, Double.valueOf(zf.getPayMoney()));
+		}
+		request.setAttribute("zje", zje);
+		request.setAttribute("kzf", kzf);
+		
+		return prefix+"addPayIndex";
+	}
+	
+	@RequestMapping("/addPayInfoRg")//人工支付
+	public String addPayInfoRg(HttpServletRequest request){
+		Arith air = new Arith();
+		List<User> ul1 = baseService.findMapBySql("select u.name name ,u.id id from tbl_user u ", new Object[]{}, new Type[]{StringType.INSTANCE}, User.class);
+		request.setAttribute("use", JSONArray.toJSONString(ul1));
+		String gclid = request.getParameter("gclid");
+		request.setAttribute("gclid", gclid);
+		
+		String gzid = request.getParameter("gzid");
+		request.setAttribute("gzid", gzid);
+		
+		String zfid = request.getParameter("zfid");
+		String lx = request.getParameter("lx");
+		request.setAttribute("zfid", zfid);
+		request.setAttribute("lx", lx);
+		
+		List<SalaryDetail> gzmxs = baseService.findBySql(" select * from tbl_salary_detail where salary_id ='"+gzid+"'", SalaryDetail.class);
+		double zje = 0.0;
+		for(SalaryDetail gzmx:gzmxs){
+			zje =air.add(zje, Double.valueOf(gzmx.getActual()));
+		}
+		String sql = " select * from tbl_volumePay where volumeId='"+gzid+"' and payType='10' and status<>'40'";
+		List<VolumePay> zfs = baseService.findBySql(sql, VolumePay.class);
+		double kzf = zje;
+		for(VolumePay zf:zfs){
+			kzf = air.sub(kzf, Double.valueOf(zf.getPayMoney()));
+		}
+		request.setAttribute("zje", zje);
+		request.setAttribute("kzf", kzf);
+		ProjectVolume pv = baseService.get(ProjectVolume.class, gclid);
+		Labor ht = baseService.get(Labor.class, pv.getLaborID());
+		request.setAttribute("bm", pv.getProjectName()+"--"+ht.getConstructionTeam());
+		
+		
+		return prefix+"addPayIndexRg";
+	}
+	
+	@RequestMapping("/auidLook/{id}")//审核查看页
+	public String auidLook(@PathVariable("id") String id,HttpServletRequest request){
+		Arith air = new Arith(); 
+		VolumePay zf = baseService.get(VolumePay.class, id);
+		if("10".equals(zf.getPayType())){//人工
+			List<User> ul1 = baseService.findMapBySql("select u.name name ,u.id id from tbl_user u ", new Object[]{}, new Type[]{StringType.INSTANCE}, User.class);
+			request.setAttribute("use", JSONArray.toJSONString(ul1));
+			request.setAttribute("zfid", id);
+			String gzid = zf.getVolumeId();
+			
+			
+			List<SalaryDetail> gzmxs = baseService.findBySql(" select * from tbl_salary_detail where salary_id ='"+gzid+"'", SalaryDetail.class);
+			double zje = 0.0;
+			for(SalaryDetail gzmx:gzmxs){
+				zje =air.add(zje, Double.valueOf(gzmx.getActual()));
+			}
+			String sql = " select * from tbl_volumePay where volumeId='"+gzid+"' and payType='10' and status<>'40'";
+			List<VolumePay> zfs = baseService.findBySql(sql, VolumePay.class);
+			double kzf = zje;
+			for(VolumePay z:zfs){
+				kzf = air.sub(kzf, Double.valueOf(z.getPayMoney()));
+			}
+			request.setAttribute("zje", zje);
+			request.setAttribute("kzf", kzf);
+			request.setAttribute("lx", "look");
+			return prefix+"addPayIndexRg";
+		}else{
+			List<User> ul1 = baseService.findMapBySql("select u.name name ,u.id id from tbl_user u ", new Object[]{}, new Type[]{StringType.INSTANCE}, User.class);
+			request.setAttribute("use", JSONArray.toJSONString(ul1));
+			request.setAttribute("gclid", zf.getVolumeId());
+			request.setAttribute("type", zf.getPayType());
+			
+			request.setAttribute("zfid", id);
+			request.setAttribute("lx", "look");
+			
+			//算出总金额和可支付金额
+			ProjectVolume pv = baseService.get(ProjectVolume.class, zf.getVolumeId());
+			Labor ht = baseService.get(Labor.class, pv.getLaborID());
+			request.setAttribute("bm", pv.getProjectName()+"--"+ht.getConstructionTeam());
+			String sql = " select * from tbl_volumePay where volumeId='"+zf.getVolumeId()+"' and payType='"+zf.getPayType()+"' and status<>'40'";
+			List<VolumePay> zfs = baseService.findBySql(sql, VolumePay.class);
+			double zje = 0.0;
+			if("20".equals(zf.getPayType())){
+				zje = Double.valueOf(pv.getFinalMech());
+			}else if("30".equals(zf.getPayType())){
+				zje = Double.valueOf(pv.getFinalMat());
+			}
+			double kzf = zje;
+			for(VolumePay z:zfs){
+				kzf = air.sub(kzf, Double.valueOf(z.getPayMoney()));
+			}
+			request.setAttribute("zje", zje);
+			request.setAttribute("kzf", kzf);
+			return prefix+"addPayIndex";
+		}
+		
+	}
+	
+	
 	@RequestMapping("/payMoney")
 	public String payMoney(HttpServletRequest request){
 		List<User> ul1 = baseService.findMapBySql("select u.name name ,u.id id from tbl_user u ", new Object[]{}, new Type[]{StringType.INSTANCE}, User.class);
@@ -78,29 +254,6 @@ public class VolumePayController {
 		r.setData(zf);
 		return r;
 	}
-	@RequestMapping("/getGclxx")
-	@ResponseBody
-	public Result getGclxx(HttpServletRequest request){
-		String gclid = request.getParameter("gclid");
-		String type = request.getParameter("type");
-		ProjectVolume gcl = baseService.get(ProjectVolume.class, gclid);
-		Labor ht = baseService.get(Labor.class, gcl.getLaborID());
-		
-		VolumePay zf = new VolumePay();
-		zf.setDepartment(gcl.getProjectName()+"-"+ht.getConstructionTeam());
-		if("10".equals(type)){//人工
-			zf.setPayMoney(gcl.getFinalLabour());
-		}else if("20".equals(type)){//机械
-			zf.setPayMoney(gcl.getFinalMech());
-		}else if("30".equals(type)){//材料
-			zf.setPayMoney(gcl.getFinalMat());
-		}
-		zf.setPayType(type);
-		zf.setVolumeId(gclid);
-		Result r = new Result();
-		r.setData(zf);
-		return r;
-	}
 	@RequestMapping("/doPay")
 	@ResponseBody
 	public Result doPay(HttpServletRequest request,VolumePay zf){
@@ -108,12 +261,13 @@ public class VolumePayController {
 		zf.setVolumeId(gclid);
 		zf.setCreateDate(new Date());
 		zf.setCreateId(SecurityUtil.getUserId());
+		zf.setStatus("10");
 		baseService.save(zf);
 		Result r = new Result();
 		r.setSuccess(true);
 		return r;
 	}
-	@RequestMapping("/isHaveSalary")
+	/*@RequestMapping("/isHaveSalary")
 	@ResponseBody
 	public Result isHaveSalary(HttpServletRequest request){
 		Arith arith = new Arith();
@@ -136,5 +290,75 @@ public class VolumePayController {
 			r.setMessage("未生成工资数据，请先生成工资数据");
 		}
 		return r;
+	}*/
+	@RequestMapping("/delPayInfo")
+	@ResponseBody
+	public Result delPayInfo(HttpServletRequest request){
+		String id = request.getParameter("id");
+		VolumePay zf = baseService.get(VolumePay.class, id);
+		baseService.delete(zf);
+		
+		Result r = new Result();
+		r.setSuccess(true);
+		return r;
 	}
+	@RequestMapping("/updatePayInfo")
+	@ResponseBody
+	public Result updatePayInfo(HttpServletRequest request,VolumePay nzf){
+		String zfid = request.getParameter("zfid");
+		VolumePay zf = baseService.get(VolumePay.class, zfid);
+		zf.setFzrId(nzf.getFzrId());
+		zf.setPayMoney(nzf.getPayMoney());
+		zf.setPayWay(nzf.getPayWay());
+		baseService.update(zf);
+		Result r = new Result();
+		r.setSuccess(true);
+		return r;
+	}
+	
+	@RequestMapping("/start")
+	@ResponseBody
+	public Result start(String id) {
+		VolumePay zf = baseService.get(VolumePay.class, id);
+		
+		User user = SecurityUtil.getUser();
+
+		zf.setStatus("20");
+
+		baseService.update(zf);
+		String zflx="";
+		if("10".equals(zf.getPayType())){
+			zflx = "人工费";
+		}else if("20".equals(zf.getPayType())){
+			zflx = "机械费";
+		}else if("30".equals(zf.getPayType())){
+			zflx = "材料费";
+		}
+		String gclid = zf.getVolumeId();
+		String qdmc = "";
+		if("10".equals(zf.getPayType())){//人工
+			List<SalaryVolume> gxs = baseService.findBySql("select * from tbl_salary_volume where salary_id='"+gclid+"'", SalaryVolume.class);
+			if(gxs.size()>0){
+				ProjectVolume gcl = baseService.get(ProjectVolume.class, gxs.get(0).getVolumeID());
+				qdmc = gcl.getProjectName();
+			}
+		}else{//机械和材料
+			ProjectVolume gcl = baseService.get(ProjectVolume.class, gclid);
+			qdmc = gcl.getProjectName();
+		}
+
+        String name =qdmc + " 支付" + zflx + "【审核】";
+
+        // businessKey
+        String businessKey = zf.getId();
+
+        // 配置流程变量
+        Map<String, Object> variables = new HashMap<>();
+        variables.put(Constants.VAR_APPLYUSER_NAME, user.getName());
+        variables.put(Constants.VAR_BUSINESS_KEY, businessKey);
+        variables.put("taskName", name);
+
+        // 启动流程
+        return runtimePageService.startProcessInstanceByKey("volumePay", name, variables, user.getId(), businessKey);
+    }
 }
---
+++ AdminEAP-web/src/main/java/com/radish/master/listener/VolumePayTaskCompleteListener.java
@@ -0,0 +1,147 @@
+/**
+ * 版权所有 (c) 2018，中金支付有限公司  
+ */
+package com.radish.master.listener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+import com.cnpc.framework.activiti.pojo.Constants;
+import com.cnpc.framework.base.service.BaseService;
+import com.radish.master.entity.Labor;
+import com.radish.master.entity.ProjectVolume;
+import com.radish.master.entity.project.Salary;
+import com.radish.master.entity.project.SalaryDetail;
+import com.radish.master.entity.project.SalaryVolume;
+import com.radish.master.entity.volumePay.VolumePay;
+import com.radish.master.system.Arith;
+import com.radish.master.system.SpringUtil;
+
+/**
+* 类说明
+* 
+* <pre>
+* Modify Information:
+* Author        Date          Description
+* ============ =========== ============================
+* dongyan      2018年4月13日    Create this file
+* </pre>
+* 
+*/
+
+public class VolumePayTaskCompleteListener implements TaskListener {
+
+    private static final long serialVersionUID = 4294247719992051739L;
+
+    @Override
+    public void notify(DelegateTask delegateTask) {
+    	Arith ari = new Arith();
+        String eventName = delegateTask.getEventName();
+        if(EVENTNAME_COMPLETE.equals(eventName)){
+             String businessKey = delegateTask.getVariable(Constants.VAR_BUSINESS_KEY).toString();
+             BaseService baseService = (BaseService)SpringUtil.getObject("baseActServer");
+             Map<String, Object> params = new HashMap<String, Object>();
+             params.put("id", businessKey);
+             VolumePay zf = baseService.get("from VolumePay where id=:id", params);
+             
+             List<ProjectVolume> zzs = new ArrayList<ProjectVolume>();
+             double zje = 0.0;
+             String gclid = zf.getVolumeId();
+             if("10".equals(zf.getPayType())){//人工
+            	List<SalaryDetail> gzmxs = baseService.findBySql(" select * from tbl_salary_detail where salary_id ='"+gclid+"'", SalaryDetail.class);
+         		
+         		for(SalaryDetail gzmx:gzmxs){
+         			zje =ari.add(zje, Double.valueOf(gzmx.getActual()));
+         		}
+            	List<SalaryVolume> gxs = baseService.findBySql("select * from tbl_salary_volume where salary_id='"+gclid+"'", SalaryVolume.class);
+     			for(SalaryVolume gx:gxs){
+     				ProjectVolume gcl = baseService.get(ProjectVolume.class, gx.getVolumeID());
+     				zzs.add(gcl);
+     			}
+     		}else{//机械和材料
+     			ProjectVolume gcl = baseService.get(ProjectVolume.class, gclid);
+     			if("20".equals(zf.getPayType())){//机械
+                	zje = ari.add(zje, Double.valueOf(gcl.getFinalMech()));
+                }else if("30".equals(zf.getPayType())){//材料
+                	zje = ari.add(zje,Double.valueOf(gcl.getFinalMat()));
+                }
+     		}
+             Double yzje = zje;
+             //计算是否已经完全支付，并修改工程量的状态
+             if("10".equals(zf.getPayType())){
+            	 String sql = " select * from tbl_volumePay where volumeId='"+zf.getVolumeId()+"' and payType='10' and status='30'";
+          		List<VolumePay> zfs = baseService.findBySql(sql, VolumePay.class);
+          		for(VolumePay z:zfs){
+         			zje = ari.sub(zje, Double.valueOf(z.getPayMoney()));
+         		}
+             }else if("20".equals(zf.getPayType())){//机械
+            	 String sql = " select * from tbl_volumePay where volumeId='"+zf.getVolumeId()+"' and payType='20' and status='30'";
+         		List<VolumePay> zfs = baseService.findBySql(sql, VolumePay.class);
+         		for(VolumePay z:zfs){
+         			zje = ari.sub(zje, Double.valueOf(z.getPayMoney()));
+         		}
+             }else if("30".equals(zf.getPayType())){//材料
+            	 String sql = " select * from tbl_volumePay where volumeId='"+zf.getVolumeId()+"' and payType='30' and status='30'";
+          		List<VolumePay> zfs = baseService.findBySql(sql, VolumePay.class);
+          		for(VolumePay z:zfs){
+         			zje = ari.sub(zje, Double.valueOf(z.getPayMoney()));
+         		}
+             }
+             
+             if("true".equalsIgnoreCase(delegateTask.getVariable("approved").toString())){
+            	 zf.setStatus("30");
+            	 zje = ari.sub(zje, Double.valueOf(zf.getPayMoney()));
+             }else if("false".equalsIgnoreCase(delegateTask.getVariable("approved").toString())){
+            	 String suggestion = delegateTask.getVariable("suggestion").toString();
+            	 zf.setStatus("40");
+            	 zf.setRebutReason(suggestion);
+             }
+             
+             if(zje>0&&zje<yzje){
+       			for(ProjectVolume g:zzs){
+       				if("10".equals(zf.getPayType())){
+         				g.setLabourStatus("10");
+                    }else if("20".equals(zf.getPayType())){//机械
+                    	g.setMechStatus("10");
+                    }else if("30".equals(zf.getPayType())){//材料
+                    	g.setMatStatus("10");
+                    }
+       				
+       				baseService.update(g);
+       			}
+       		}else if(zje==0){
+       			for(ProjectVolume g:zzs){
+       				if("10".equals(zf.getPayType())){
+         				g.setLabourStatus("20");
+                    }else if("20".equals(zf.getPayType())){//机械
+                    	g.setMechStatus("20");
+                    }else if("30".equals(zf.getPayType())){//材料
+                    	g.setMatStatus("20");
+                    }
+       				baseService.update(g);
+       			}
+       		}else{
+       			for(ProjectVolume g:zzs){
+       				if("10".equals(zf.getPayType())){
+         				g.setLabourStatus(null);
+                    }else if("20".equals(zf.getPayType())){//机械
+                    	g.setMechStatus(null);
+                    }else if("30".equals(zf.getPayType())){//材料
+                    	g.setMatStatus(null);
+                    }
+       				baseService.update(g);
+       			}
+       		}
+             baseService.save(zf);
+             
+             
+        }
+
+    }
+    
+}
