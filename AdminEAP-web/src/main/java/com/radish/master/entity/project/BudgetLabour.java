74c117bbea8dc1d237f640ba16707820d499e547
Squama <tonydong20@gmail.com>
2018-05-25 17:58:30 +0800

    部分提交

---
+++ AdminEAP-web/src/main/java/com/radish/master/entity/project/BudgetLabour.java
@@ -0,0 +1,102 @@
+/**
+ * 版权所有 (c) 2018，中金支付有限公司  
+ */
+package com.radish.master.entity.project;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.cnpc.framework.annotation.Header;
+import com.cnpc.framework.base.entity.BaseEntity;
+
+/**
+ * 类说明
+ * 
+ * <pre>
+* Modify Information:
+* Author        Date          Description
+* ============ =========== ============================
+* dongyan      2018年5月25日    Create this file
+ * </pre>
+ * 
+ */
+@Entity
+@Table(name = "tbl_budget_labour")
+public class BudgetLabour extends BaseEntity{
+
+    private static final long serialVersionUID = -3714495444403163102L;
+
+    @Header(name = "预算明细编号")
+    @Column(name = "budget_tx_id")
+    private String budgetTxID;
+
+    @Header(name = "项目编号")
+    @Column(name = "project_id")
+    private String projectID;
+
+    @Header(name = "预算编号")
+    @Column(name = "budget_no")
+    private String budgetNo;
+
+    @Header(name = "人工费名称")
+    @Column(name = "labour_name")
+    private String labourName;
+
+    @Header(name = "市场估计价")
+    @Column(name = "forecast_price")
+    private String forecastPrice;
+
+    @Header(name = "消耗量")
+    @Column(name = "labour_quantity")
+    private String labourQuantity;
+
+    public String getBudgetTxID() {
+        return budgetTxID;
+    }
+
+    public void setBudgetTxID(String budgetTxID) {
+        this.budgetTxID = budgetTxID;
+    }
+
+    public String getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(String projectID) {
+        this.projectID = projectID;
+    }
+
+    public String getBudgetNo() {
+        return budgetNo;
+    }
+
+    public void setBudgetNo(String budgetNo) {
+        this.budgetNo = budgetNo;
+    }
+
+    public String getLabourName() {
+        return labourName;
+    }
+
+    public void setLabourName(String labourName) {
+        this.labourName = labourName;
+    }
+
+    public String getForecastPrice() {
+        return forecastPrice;
+    }
+
+    public void setForecastPrice(String forecastPrice) {
+        this.forecastPrice = forecastPrice;
+    }
+
+    public String getLabourQuantity() {
+        return labourQuantity;
+    }
+
+    public void setLabourQuantity(String labourQuantity) {
+        this.labourQuantity = labourQuantity;
+    }
+
+}
---
+++ AdminEAP-web/src/main/java/com/radish/master/entity/project/BudgetMech.java
@@ -0,0 +1,102 @@
+/**
+ * 版权所有 (c) 2018，中金支付有限公司  
+ */
+package com.radish.master.entity.project;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.cnpc.framework.annotation.Header;
+import com.cnpc.framework.base.entity.BaseEntity;
+
+/**
+ * 类说明
+ * 
+ * <pre>
+* Modify Information:
+* Author        Date          Description
+* ============ =========== ============================
+* dongyan      2018年5月25日    Create this file
+ * </pre>
+ * 
+ */
+@Entity
+@Table(name = "tbl_budget_mech")
+public class BudgetMech extends BaseEntity {
+
+    private static final long serialVersionUID = -1783473822775735079L;
+
+    @Header(name = "预算明细编号")
+    @Column(name = "budget_tx_id")
+    private String budgetTxID;
+
+    @Header(name = "项目编号")
+    @Column(name = "project_id")
+    private String projectID;
+
+    @Header(name = "预算编号")
+    @Column(name = "budget_no")
+    private String budgetNo;
+
+    @Header(name = "人工费名称")
+    @Column(name = "labour_name")
+    private String labourName;
+
+    @Header(name = "台班单价")
+    @Column(name = "mech_price")
+    private String mechPrice;
+
+    @Header(name = "台班消耗量")
+    @Column(name = "mech_quantity")
+    private String mechQuantity;
+
+    public String getBudgetTxID() {
+        return budgetTxID;
+    }
+
+    public void setBudgetTxID(String budgetTxID) {
+        this.budgetTxID = budgetTxID;
+    }
+
+    public String getProjectID() {
+        return projectID;
+    }
+
+    public void setProjectID(String projectID) {
+        this.projectID = projectID;
+    }
+
+    public String getBudgetNo() {
+        return budgetNo;
+    }
+
+    public void setBudgetNo(String budgetNo) {
+        this.budgetNo = budgetNo;
+    }
+
+    public String getLabourName() {
+        return labourName;
+    }
+
+    public void setLabourName(String labourName) {
+        this.labourName = labourName;
+    }
+
+    public String getMechPrice() {
+        return mechPrice;
+    }
+
+    public void setMechPrice(String mechPrice) {
+        this.mechPrice = mechPrice;
+    }
+
+    public String getMechQuantity() {
+        return mechQuantity;
+    }
+
+    public void setMechQuantity(String mechQuantity) {
+        this.mechQuantity = mechQuantity;
+    }
+
+}
