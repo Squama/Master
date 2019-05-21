e23a7d2a30f245092e180f6b365d6f3ea5f87fe3
wzh0209 <32840049+wzh0209@users.noreply.github.com>
2019-05-21 17:18:43 +0800

    工资支付审核 和 个税支付审核 记录审核意见

--- AdminEAP-web/src/main/java/com/radish/master/listener/SalaryPayTaskCompleteListener.java
+++ AdminEAP-web/src/main/java/com/radish/master/listener/SalaryPayTaskCompleteListener.java
@@ -4,6 +4,7 @@
 package com.radish.master.listener;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,9 +16,11 @@ import com.cnpc.framework.activiti.pojo.Constants;
 import com.cnpc.framework.base.entity.User;
 import com.cnpc.framework.base.entity.UserRole;
 import com.cnpc.framework.base.service.BaseService;
+import com.cnpc.framework.utils.SecurityUtil;
 import com.radish.master.entity.Labor;
 import com.radish.master.entity.ProjectVolume;
 import com.radish.master.entity.UserLeave;
+import com.radish.master.entity.common.ActivitiSuggestion;
 import com.radish.master.entity.project.Salary;
 import com.radish.master.entity.project.SalaryDetail;
 import com.radish.master.entity.project.SalaryVolume;
@@ -52,6 +55,29 @@ public class SalaryPayTaskCompleteListener implements TaskListener {
         if(EVENTNAME_COMPLETE.equals(eventName)){
              String businessKey = delegateTask.getVariable(Constants.VAR_BUSINESS_KEY).toString();
              BaseService baseService = (BaseService)SpringUtil.getObject("baseActServer");
+             
+             String taskDefinitionKey = delegateTask.getTaskDefinitionKey();
+             String suggestion = delegateTask.getVariable("suggestion").toString();
+             
+             String suggestionHql = "from ActivitiSuggestion where businessKey=:businessKey AND taskNode=:taskNode";
+             Map<String, Object> suggestionParams = new HashMap<>();
+             suggestionParams.put("businessKey", businessKey);
+             suggestionParams.put("taskNode", taskDefinitionKey);
+             ActivitiSuggestion as = baseService.get(suggestionHql, suggestionParams);
+             
+             if(as == null){
+                 as = new ActivitiSuggestion();
+                 as.setCreateDateTime(new Date());
+                 as.setUpdateDateTime(new Date());
+                 as.setBusinessKey(businessKey);
+                 as.setTaskNode(taskDefinitionKey);
+                 as.setApprove("true");
+             }
+             as.setSuggestion(suggestion);
+             as.setOperator(SecurityUtil.getUser().getName());
+             as.setUpdateDateTime(new Date());
+             baseService.save(as);
+             
              Map<String, Object> params = new HashMap<String, Object>();
              params.put("id", businessKey);
              Salary zf = baseService.get("from Salary where id=:id", params);
